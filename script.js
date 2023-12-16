3ff30f9a44d35ea9e5dde41f82051a1a260ebe38
ShagantiNagasree <153216600+ShagantiNagasree@users.noreply.github.com>
2023-12-16 17:02:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+alert("are you really want to enter in this page");
+console.log("hello world");
+alert("are you really want to enter in console");
+let num1=1;
+let num2=2;
+let num3=28;
+let word="word";
+console.log("sum of numbers is :")
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num3%num2);
+console.log(word);
+console.log(num1>num2);
+console.log(num1<num2);
+console.log(num1>=num2);
+console.log(num1<=num2);
+console.log(num1==num2);
+console.log(num1>num2&&num1<num2);
+console.log(num1>num2||num1<num2);
+function add(){
+    console.log("ADDITION FUNCTION IS CALLED");
+    console.log(num1+num2);
+}
+add();
